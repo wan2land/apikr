29473f9b220c91a700afa5722b103723a2735f07
Changwan Jun <im@wani.kr>
2017-07-17 15:08:47 +0900

    add composer.json

---
+++ build/split-full.sh
@@ -0,0 +1,31 @@
+
+split_parallel() {
+    DIRPATH=$1
+    PACKAGENAME=$2
+    HEAD=$3
+    TAG=$4
+    mkdir $PACKAGENAME
+    pushd $PACKAGENAME
+    git subsplit init git@github.com:apikr/apikr.git
+    if [ ! -z "$TAG" ]; then
+        git subsplit publish --heads="$HEAD" --tags="$TAG" src/$DIRPATH:git@github.com:apikr/$PACKAGENAME.git
+    else
+        git subsplit publish --heads="$HEAD" --no-tags src/$DIRPATH:git@github.com:apikr/$PACKAGENAME.git
+    fi
+    popd
+    rm -rf $PACKAGENAME
+}
+
+TAG=$1
+PIDS=()
+
+split_parallel Api              api                 "master" $TAG & PIDS+=($!)
+split_parallel Aligo/Sms        aligo-sms           "master" $TAG & PIDS+=($!)
+split_parallel ApiStore/Sms     apistore-sms        "master" $TAG & PIDS+=($!)
+split_parallel Paygate/Seyfert  paygate-seyfert     "master" $TAG & PIDS+=($!)
+split_parallel SKPlanet/TMap    skplanet-tmap       "master" $TAG & PIDS+=($!)
+
+for PID in "${PIDS[@]}"
+do
+	wait $PID
+done
